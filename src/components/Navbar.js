e5490032076a49c36b7e96c46b6f6d8ce1e5b7e6
Regynald Zambrano <zreyleo@gmail.com>
2021-09-15 21:26:49 -0500

    estilos al navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,18 +7,24 @@ const Navbar = () => {
     const { sessionRole } = useContext(Context)
 
     return (
-        <nav className="w-full">
-            {
-                !sessionRole ? (
-                    <>
-                        <Link to="/login/administrador">Login Administrador</Link>
+        <nav className="w-full bg-secondary">
+            <div className="container">
+                <div className="row">
+                    <div className="py-2 col-md-4 offset-8 d-flex justify-content-between">
+                        {
+                            !sessionRole ? (
+                                <>
+                                    <Link className="text-white" to="/login/administrador">Login Administrador</Link>
 
-                        <Link to="/login/empleados">Login Empleado</Link>
-                    </>
-                ) : (
-                    <Link to="/">Cerrar Sesi&oacute;n</Link>
-                )
-            }
+                                    <Link className="text-white" to="/login/empleados">Login Empleado</Link>
+                                </>
+                            ) : (
+                                <Link className="text-white" to="/">Cerrar Sesi&oacute;n</Link>
+                            )
+                        }
+                    </div>
+                </div>
+            </div>
         </nav >
     );
 }
